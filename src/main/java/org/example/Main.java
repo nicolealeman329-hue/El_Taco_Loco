13c221d415aa7a3d0aba96914cfd0dcc7945fe11
nicolecarranza <nicolealeman329@gmail.com>
2026-05-28 13:49:17 -0400

    intial commit.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import ui.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+}
